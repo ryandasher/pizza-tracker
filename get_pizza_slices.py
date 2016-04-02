4ef852a794bb946a9eb4828efc0c1ffabef2fda7
Ryan Asher <ryandeanasher@gmail.com>
2016-04-02 19:20:11 -0400

    Fix lambda handler bugs:
    Make sure we're passing the context object to our lambda handler functions. Properly import the boto
    library. Pass the proper variables in the correct format to the SDB methods.

--- get_pizza_slices.py
+++ get_pizza_slices.py
@@ -1,16 +1,29 @@
-import boto, json
+import boto3, json
 
 sdb = boto3.client('sdb')
 
-def lambda_handler():
+def lambda_handler(event, context):
 	"""
 	Handler for retrieving data from Simple DB.
+
+	Args:
+	event -- Takes the empty request body (Dictionary).
+	context -- AWS context for the request (Object).
 	"""
 	data = {}
 
 	try:
-		data['Ryan'] = sdb.get_attributes('pizza', 'Ryan', 'Slices')
-		data['Sharon'] = sdb.get_attributes('pizza', 'Sharon', 'Slices')
+		data['Ryan'] = sdb.get_attributes(
+			DomainName='pizza',
+			ItemName='Ryan',
+			AttributeNames=['Slices']
+		)
+
+		data['Sharon'] = sdb.get_attributes(
+			DomainName='pizza',
+			ItemName='Sharon',
+			AttributeNames=['Slices']
+		)
 	except:
 		return json.dumps({'Error': 'Data could not be retrieved.'})
 
--- post_pizza_slices.py
+++ post_pizza_slices.py
@@ -1,23 +1,32 @@
-import boto, json
+import boto3, json
 
 sdb = boto3.client('sdb')
 
-def lambda_handler(data):
+def lambda_handler(data, context):
 	"""
 	Handler for posting data to SimpleDB.
 
 	Args:
 	data -- Data to be stored (Dictionary).
+	context -- AWS context for the request (Object).
 	"""
 	item_name = data['Name']
 
 	if data['Password'] and data['Password'] == 'INSERT PASSWORD':
 		try:
-			sdb.put_attributes(DomainName='pizza', ItemName=item_name,
-							   Attributes={'Slices': data['Slices']})
+			sdb.put_attributes(
+				DomainName='pizza',
+				ItemName=item_name,
+				Attributes=[{
+					'Name': 'Slices',
+					'Value': str(data['Slices']),
+					'Replace': True
+				}])
+
 			return json.dumps({'Success': 'Your data was submitted!'})
 		except:
 			return json.dumps({'Error': 'Your data was not submitted.'})
 	else:
-		return json.dumps({'Error':
-						   'Ah ah ah, you didn\'t say the magic word.'})
+		return json.dumps({
+			'Error': 'Ah ah ah, you didn\'t say the magic word.'
+		})
