53dd02a40c983672428739a99338903965fa8593
Ryan Asher <ryandeanasher@gmail.com>
2016-04-05 00:40:36 -0400

    Pizza icon display logic:
    Write logic that gets the amount of pizza slices eaten from the database, and then
    determines how many full pizza pie icons should be displayed vs. how many pizza slice
    icons should be displayed using division and modulo.

--- static/js/pizza.js
+++ static/js/pizza.js
@@ -2,6 +2,14 @@
 	var httpRequest;
 	makeApiRequest('https://bio9mswh9k.execute-api.us-west-2.amazonaws.com/Prod/pizzaslices', 'GET', null);
 
+	/**
+	 * Make an AWS API Gateway request.
+	 *
+	 * Args:
+	 * url -- Endpoint to request (string).
+	 * method -- GET or POST, must be capitalized (string).
+	 * body -- Data to pass to request (string).
+	*/
 	function makeApiRequest(url, method, body) {
 		httpRequest = new XMLHttpRequest();
 
@@ -10,21 +18,50 @@
 			return false;
 		}
 
-		httpRequest.onreadystatechange = storeResponse;
+		httpRequest.onreadystatechange = parseResponse;
 		httpRequest.open(method, url);
 		httpRequest.send(body);
 	}
 
-	function storeResponse() {
+	/**
+	 * Determine the result of a request.
+	*/
+	function parseResponse() {
 		if (httpRequest.readyState === XMLHttpRequest.DONE) {
 			if (httpRequest.status === 200) {
-				resp = JSON.parse(httpRequest.responseText);
-				// TODO: Just testing something.
-				// document.getElementsByName("ryan_slices")[0].placeholder=resp["Ryan"]["Attributes"][0]["Value"];
-				// document.getElementsByName("sharon_slices")[0].placeholder=resp["Sharon"]["Attributes"][0]["Value"];
+				var resp = JSON.parse(httpRequest.responseText);
+				displayPizzaIcons(resp['Sharon']['Attributes'][0]['Value'], 'sharon');
+				displayPizzaIcons(resp['Ryan']['Attributes'][0]['Value'], 'ryan');
 			} else {
 				console.log('There was a problem with the request.');
 			}
+		}
+	}
+
+	/**
+	 * Display the appropriate number of pizza pie icons, or pizza
+	 * slice icons.
+	 *
+	 * Args:
+	 * slices -- Amount of pizza slices eaten (string).
+	 * person -- Person who ate the pizza slices (string).
+	*/
+	function displayPizzaIcons(slices, person) {
+		var slices = parseInt(slices);
+		var pizza_div = document.getElementById(person);
+
+		var pies = slices / 8;
+		for (; pies >= 1; pies--) {
+			var pizza_image = document.createElement('img');
+			pizza_image.setAttribute('src', 'static/images/pizza_pie.png');
+			pizza_div.appendChild(pizza_image);
+		}
+
+		var slices = slices % 8;
+		for (; slices >= 1; slices--) {
+			var pizza_image = document.createElement('img');
+			pizza_image.setAttribute('src', 'static/images/pizza_slice.png');
+			pizza_div.appendChild(pizza_image);
 		}
 	}
 })();
