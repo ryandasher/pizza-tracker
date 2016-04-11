dcbc37a4f8838ef7156e26bc8abb7d91baac7f49
Ryan Asher <ryandeanasher@gmail.com>
2016-04-10 20:50:24 -0400

    GET pizza logic:
    Refactor the GET request for retrieving the pizza slices from SimpleDB, and when the logic
    that determines how many pizza slices or pizza pies display.

--- static/js/pizza.js
+++ static/js/pizza.js
@@ -1,67 +1,88 @@
-(function() {
-	var httpRequest;
-	makeApiRequest('https://bio9mswh9k.execute-api.us-west-2.amazonaws.com/Prod/pizzaslices', 'GET', null);
+var endpoint = 'https://bio9mswh9k.execute-api.us-west-2.amazonaws.com/Prod/pizzaslices';
 
-	/**
-	 * Make an AWS API Gateway request.
-	 *
-	 * Args:
-	 * url -- Endpoint to request (string).
-	 * method -- GET or POST, must be capitalized (string).
-	 * body -- Data to pass to request (string).
-	*/
-	function makeApiRequest(url, method, body) {
-		httpRequest = new XMLHttpRequest();
+/**
+* Make an AWS API Gateway request.
+*
+* Args:
+* url -- Endpoint to request (string).
+* method -- GET or POST, must be capitalized (string).
+* body -- Data to pass to request (string).
+*/
+function makeApiRequest(url, method, body, callback) {
+	var response, httpRequest;
 
-		if (!httpRequest) {
-			console.log('Can\'t create an XMLHTTP Request.');
-			return false;
-		}
+	httpRequest = new XMLHttpRequest();
 
-		httpRequest.onreadystatechange = parseResponse;
-		httpRequest.open(method, url);
-		httpRequest.send(body);
+	if (!httpRequest) {
+		console.log('Can\'t create an XMLHTTP Request.');
+		return false;
 	}
 
-	/**
-	 * Determine the result of a request.
-	*/
-	function parseResponse() {
-		if (httpRequest.readyState === XMLHttpRequest.DONE) {
-			if (httpRequest.status === 200) {
-				var resp = JSON.parse(httpRequest.responseText);
-				displayPizzaIcons(resp['Sharon']['Attributes'][0]['Value'], 'sharon');
-				displayPizzaIcons(resp['Ryan']['Attributes'][0]['Value'], 'ryan');
-			} else {
-				console.log('There was a problem with the request.');
+	httpRequest.onreadystatechange = function() {
+		if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+			callback(JSON.parse(httpRequest.responseText));
+		}
+	}
+
+	httpRequest.open(method, url);
+	httpRequest.send(body);
+}
+
+
+/**
+* Display the appropriate number of pizza pie icons, or pizza
+* slice icons.
+*
+* Args:
+* response -- JSON object returned by GET request (object).
+*/
+function displayPizzaIcons(response) {
+	for (var person in response) {
+		if (response.hasOwnProperty(person)) {
+			var slices = parseInt(response[person]['Attributes'][0]['Value']);
+			var pizza_div = document.getElementById(person.toLowerCase());
+
+			// First we divide the total slices by eight to figure out how many
+			// whole pie icons to display. Then we'll use modulo to check the
+			// remainder, which we'll display as single slices.
+			var pies = slices / 8;
+			for (; pies >= 1; pies--) {
+				var pizza_image = document.createElement('img');
+				pizza_image.setAttribute('src', 'static/images/pizza_pie.png');
+				pizza_div.appendChild(pizza_image);
+			}
+
+			var leftover_slices = slices % 8;
+			for (; leftover_slices >= 1; leftover_slices--) {
+				var pizza_image = document.createElement('img');
+				pizza_image.setAttribute(
+					'src',
+					'static/images/pizza_slice.png'
+				);
+				pizza_div.appendChild(pizza_image);
 			}
 		}
 	}
+}
 
-	/**
-	 * Display the appropriate number of pizza pie icons, or pizza
-	 * slice icons.
-	 *
-	 * Args:
-	 * slices -- Amount of pizza slices eaten (string).
-	 * person -- Person who ate the pizza slices (string).
-	*/
-	function displayPizzaIcons(slices, person) {
-		var slices = parseInt(slices);
-		var pizza_div = document.getElementById(person);
 
-		var pies = slices / 8;
-		for (; pies >= 1; pies--) {
-			var pizza_image = document.createElement('img');
-			pizza_image.setAttribute('src', 'static/images/pizza_pie.png');
-			pizza_div.appendChild(pizza_image);
-		}
-
-		var slices = slices % 8;
-		for (; slices >= 1; slices--) {
-			var pizza_image = document.createElement('img');
-			pizza_image.setAttribute('src', 'static/images/pizza_slice.png');
-			pizza_div.appendChild(pizza_image);
-		}
+function submitPizza() {
+	alert("The hey?");
+	var data = {
+		'Sharon': parseInt(document.getElementsByName('sharon_slices')[0].value),
+		'Ryan': parseInt(document.getElementsByName('ryan_slices')[0].value),
+		'Password': document.getElementsByName('password')[0].value
 	}
-})();
+
+	makeApiRequest(endpoint, 'POST', data, displaySubmitMessage);
+}
+
+
+function displaySubmitMessage() {
+	alert("Just a test alert now");
+}
+
+
+if (document.getElementById('pizza-table')) {
+	makeApiRequest(endpoint, 'GET', null, displayPizzaIcons);
+}
