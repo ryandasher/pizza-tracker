76a02130db4a3d3a315c7dbae47d1c638acdde09
Ryan Asher <ryandeanasher@gmail.com>
2016-04-25 21:53:06 -0400

    Deployment tweaks:
    Update deployment script to exclude more unnecessary files. Update paths to static files
    not included in the Pizza Tracker project.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ aws s3 sync . s3://ryandasher.com/projects/pizza-tracker \
 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
 full=emailaddress=$1 --exclude="*.git/*" --exclude="README.md" \
 --exclude="LICENSE.txt" --exclude="deploy.sh" --exclude=".gitignore" \
---exclude="*.DS_Store"
+--exclude="*.DS_Store" --exclude="*.py" --exclude="*.pyc"
